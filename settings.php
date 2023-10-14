9302f232c86705876ef8bd39096058dbd119c8d0
Philipp Freilinger <philipp@freilinger.org>
2023-10-14 22:31:56 +0200

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Array of car types indexed by IDs.
+ */
+$cartypes = array(
+    0 => 'McLaren F1',
+    1 => 'Ferrari F50',
+    2 => 'Ferrari 355 F1',
+    3 => 'Ford GT90',
+    4 => 'Ford Indigo',
+    5 => 'Ford Mach III',
+    6 => 'Jaguar XJ220',
+    7 => 'Lotus GT1',
+    8 => 'Lotus Esprit V8',
+    9 => 'ItalDesign Nazca C2',
+    10 => 'ItalDesign Cala',
+    11 => 'Isdera 112i',
+    12 => 'Bomber BFS',
+    13 => 'Tombstone',
+    14 => 'FZR 2000',
+);
+
+
+/**
+ * Array of car types indexed by IDs.
+ */
+$skill_levels = array(
+    0 => 'sim',
+    1 => 'wild',
+    2 => 'arc',
+);
+
+/**
+ * Array of track names indexed by Filenames.
+ */
+$tracks = array(
+    'oval.stf'=>'Proving Grounds',
+    'oz.stf'=>'Outback',
+    'last.stf'=>'Last Resort',
+    'nort.stf'=>'North Country',
+    'pac.stf'=>'Pacific Spirit',
+    'med.stf'=>'Mediterraneo',
+    'myst.stf'=>'Mystic Peaks',
+    'mono.stf'=>'Monolithic Studios',
+);
+
+/**
+ * Array of track names indexed by IDs.
+ */
+$track_ids = array(
+    0=>'Proving Grounds',
+    2=>'Outback',
+    4=>'Last Resort',
+    5=>'North Country',
+    3=>'Pacific Spirit',
+    6=>'Mediterraneo',
+    7=>'Mystic Peaks',
+    8=>'Monolithic Studios',
+);
+
+/**
+ * Array of lap counts names indexed by lap count.
+ */
+$lap_counts = array(
+    2=>'2_laps',
+    4=>'4_laps',
+    8=>'8_laps',
+);
+
+/**
+ * Array of track direction indexed by IDs.
+ */
+$lap_directions = array(
+    0=>'forward',
+    1=>'backwards',
+    
+);
+
+/**
+ * Array of track direction (mirrored or not) indexed by IDs.
+ */
+$lap_directions2 = array(
+    0=>'no',
+    1=>'yes',
+    
+);
+
+?>
